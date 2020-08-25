2adac631453ef020846ba036464626af814cdac4
Suhas R N <uzumakinaruto1582@gmail.com>
2020-08-25 22:09:06 +0530

    Implemented basic validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,14 @@
+const {validationResult} = require('express-validator');
+
 const User = require('../models/User');
 
 exports.signup = (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(422).json({
+            error: errors.array(),
+        });
+    }
     const user = new User(req.body);
     user.save((err, user) => {
         if(err) {
@@ -8,6 +16,11 @@ exports.signup = (req, res) => {
                 err: err,
             });
         }
-        res.json(user);
+        res.json({
+            name: user.first_name,
+            phone: user.phone,
+            email: user.email,
+            id: user._id
+        });
     })
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const {body} = require('express-validator');
 
 const {signup} = require('../controllers/auth');
 
 const router = express.Router();
 
-router.post("/signup", signup);
+router.post("/signup",[
+    body('email', "Please enter a valid email").isEmail(),
+    body('phone', "Please enter a valid phone number with 10 digits").isLength({min: 10, max: 10}),
+    body('password').isLength({min: 6}).withMessage("Password should be at least 6 characters long").matches(/\d/).withMessage("Password should contain both letters and numbers")
+], signup);
 
 module.exports = router;
